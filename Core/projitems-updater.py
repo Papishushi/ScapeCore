c547c2f9b1cb85ff1497a0abf3c748cfb50ad80b
Daniel Molinero Lucas <57540881+Papishushi@users.noreply.github.com>
2024-01-20 01:13:41 +0100

    Updated installer

---
+++ Core/projitems-updater.py
@@ -0,0 +1,41 @@
+import os
+import xml.etree.ElementTree as ET
+
+submodule_path = os.getenv('submodule_path')
+proj_items_path = os.getenv('proj_items_path')
+
+if os.path.isdir(submodule_path):
+    submodule_files = [os.path.join(dirpath, filename)
+                       for dirpath, _, filenames in os.walk(submodule_path)
+                       for filename in filenames
+                       if filename.endswith(".cs")]
+    if os.path.exists(proj_items_path):
+        ET.register_namespace('xmlns', 'http://schemas.microsoft.com/developer/msbuild/2003')
+        namespace_map = {"xmlns": "http://schemas.microsoft.com/developer/msbuild/2003"}
+
+        tree = ET.parse(proj_items_path)
+        root = tree.getroot()
+
+        files_tag = root.find(".//xmlns:ItemGroup", namespaces=namespace_map)
+
+        # Check if the tag is found
+        if files_tag is not None:
+            for file in submodule_files:
+                file_element = ET.Element("Compile", Include="\$(MSBuildThisFileDirectory)" + file)
+                files_tag.append(file_element)
+
+            # Remove the ns0: prefix from the root element and its descendants
+            for elem in root.iter():
+                if '}' in elem.tag:
+                    elem.tag = elem.tag.split('}', 1)[1]
+
+            ET.indent(tree, space="  ", level=0)
+            tree.write(proj_items_path, encoding='utf-8', xml_declaration=True)
+
+            print(f"\nSubmodule files added to {proj_items_path}")
+        else:
+            print("Error: <Compilation> tag not found in the .projitems file structure.")
+    else:
+        print(f"Error: {proj_items_path} not found.")
+else:
+    print(f"Submodule not found. No changes made to {proj_items_path}")
